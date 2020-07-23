29e8e6c50e9ff09e42b435e397123b729dd30b9c
Dara <bdaralan@gmail.com>
2020-07-22 23:43:29 -0700

    Update PersistPropertyWrapperPreview - update code to demo the updated BDPersist

--- BDProjects/BDUIKnit Preview/PersistPropertyWrapperPreview.swift
+++ BDProjects/BDUIKnit Preview/PersistPropertyWrapperPreview.swift
@@ -12,11 +12,6 @@ import BDUIKnit
 
 struct PersistPropertyWrapperPreview: View {
     
-    // For the purposes of this sample code, static is used on @BDPersist
-    // so that they can be modified in View object.
-    //
-    // Usually, these properties should be in a Settings or UserPreference object
-    
     static let nUsernameDidChange = Notification.Name("nUsernameDidChange")
     static let nProfileImageUrlDidChange = Notification.Name("nProfileImageUrlDidChange")
     
@@ -28,57 +23,41 @@ struct PersistPropertyWrapperPreview: View {
     }
     
     @BDPersist(in: .userDefaults, key: Keys.username, default: "", post: nUsernameDidChange)
-    static var username: String
+    var username: String
     
     @BDPersist(in: .userDefaults, key: Keys.profileImageUrl, default: nil, post: nProfileImageUrlDidChange)
-    static var profileImageUrl: String?
+    var profileImageUrl: String?
     
-    @State private var textFieldUsername = ""
-    @State private var notificationUsername = ""
-    
-    @State private var textFieldProfileImageUrl = ""
-    @State private var notificationProfileImageUrl = ""
-    
-    let usernamePublisher = NotificationCenter.default.publisher(for: nUsernameDidChange)
-    let profileImagePublisher = NotificationCenter.default.publisher(for: nProfileImageUrlDidChange)
+    let usernameNotification = NotificationCenter.default.publisher(for: nUsernameDidChange)
+    let profileImageNotification = NotificationCenter.default.publisher(for: nProfileImageUrlDidChange)
     
     
     var body: some View {
         Form {
             Section(header: Text("NON-OPTIONAL VALUE")) {
-                Text("Persist Username: \(Self.username)")
-                
-                Text("Notification Username: \(notificationUsername)")
-                
-                TextField("Username", text: $textFieldUsername)
-                
+                Text("Username: \(username)")
                 Button("Update Username") {
-                    Self.username = self.textFieldUsername
+                    let number = Int.random(in: 1...1000)
+                    self.username = "User #\(number)"
                 }
             }
             
             Section(header: Text("OPTIONAL VALUE")) {
-                Text("Persist Profile Image URL: \(Self.profileImageUrl ?? "nil")")
-                
-                Text("Notification Profile Image URL: \(notificationProfileImageUrl)")
-                
-                TextField("Image URL", text: $textFieldProfileImageUrl)
-                    .autocapitalization(.none)
-                    .keyboardType(.URL)
-                
+                Text("Profile Image URL: \(profileImageUrl ?? "nil")")
                 Button("Update URL") {
-                    let url = self.textFieldProfileImageUrl
-                    Self.profileImageUrl = url.isEmpty ? nil : url
+                    let number = Int.random(in: 1...1000)
+                    let isNilUrl = number.isMultiple(of: 5)
+                    self.profileImageUrl = isNilUrl ? nil : "https://image.url/\(number)"
                 }
             }
         }
-        .onReceive(usernamePublisher.receive(on: DispatchQueue.main)) { notification in
-            let username = notification.object as? String
-            self.notificationUsername = username ?? "nil"
+        .onReceive(usernameNotification) { notification in
+            let username = notification.object as? String ?? "nil"
+            print("onReceive username notification: \(username)")
         }
-        .onReceive(profileImagePublisher.receive(on: DispatchQueue.main)) { notification in
-            let url = notification.object as? String
-            self.notificationProfileImageUrl = url ?? "nil"
+        .onReceive(profileImageNotification) { notification in
+            let url = notification.object as? String ?? "nil"
+            print("onReceive profile image notification: \(url)")
         }
     }
 }
